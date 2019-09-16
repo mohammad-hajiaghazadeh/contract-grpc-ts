af40f281874b564a167834ec828ab5060f4ba33a
mohammad-hajiaghazadeh <mohammad.hajiaghazadeh@trialblaze.com>
2019-09-16 23:19:46 +0430

    add proto files

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 yarn install
 
-PROTO_ROOT="contract-grpc-spec"
+PROTO_ROOT="contract-spec"
 DIST_DIR="contract-grpc-ts"
 
-for file in  $(find ./contract-grpc-spec -type f -name *.proto)
+for file in  $(find ./contract-spec -type f -name *.proto)
   do
       echo $file
       protoc --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts --js_out=import_style=commonjs,binary:$DIST_DIR --ts_out=service=grpc-web:$DIST_DIR --proto_path=$PROTO_ROOT -I . $file
